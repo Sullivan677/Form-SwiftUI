1e7b90e05e68ffcc990047a4eb62cec3cf2247b2
Sullivan677 <decarli.sulli@gmail.com>
2020-12-07 17:45:41 +0100

    Initial Commit

---
+++ Form/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Form
+//
+//  Created by Sullivan De carli on 07/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by Sullivan De carli on 07/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
