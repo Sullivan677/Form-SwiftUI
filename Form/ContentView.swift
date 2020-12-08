63d20a9278561d280d0a00e4a45fb4de2a79031e
Sullivan677 <decarli.sulli@gmail.com>
2020-12-08 19:23:35 +0100

    First commit

--- Form/ContentView.swift
+++ Form/ContentView.swift
@@ -1,16 +1,49 @@
-//
-//  ContentView.swift
-//  Form
-//
-//  Created by Sullivan De carli on 07/12/2020.
-//
-
 import SwiftUI
-
 struct ContentView: View {
+
+    @State var titleBook = ""
+    @State var author = ""
+    @State var isExchange: Bool = true
+    @State var description = "This book is about .."
+    @State var price = ""
+    @State private var categoryIndex = 0
+    var categorySelection = ["Action", "classic","Comic Book","Fantasy","Historical","Literary Fiction","Biographies","Essays"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+        Form {
+            
+                Section(header: Text("General")) {
+                    TextField("Title", text: $titleBook)
+                    TextField("Author", text: $author)
+                    Toggle(isOn: $isExchange) {
+                        Text("I'm interested in an exhange")
+                    }
+                }
+            Section() {
+                Picker(selection: $categoryIndex, label: Text("Categorie")) {
+                    ForEach(0 ..< categorySelection.count) {
+                        Text(self.categorySelection[$0])
+                    }
+                }
+            }
+            Section(header: Text("Description")) {
+                TextEditor(text: $description)
+            }
+            
+            Section(header: Text("Price")) {
+                TextField("$0.00", text: $price)
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                Button(action: {
+                    print("submitted ..")
+                }) {
+                    Text("Publish now")
+                }
+            }
+        }.navigationTitle("Publish")
+        }
     }
 }
 
